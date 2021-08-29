88cea9a9def6a47e835b3caee7e3838e6101cbf1
RyanMKrol <ryankrol.m@gmail.com>
2021-08-29 22:57:40 +0100

    Add S3 data library

---
+++ src/modules/data/s3.js
@@ -0,0 +1,59 @@
+import AWS from 'aws-sdk';
+
+import { AWS_CREDENTIALS } from '../constants';
+
+/**
+ * Method to download file from S3 storage
+ *
+ * @param {string} bucket The bucket to fetch a file for
+ * @param {string} file The file in the bucket we're fetching
+ * @returns {Promise<JSON>} Data from a file in S3
+ */
+async function downloadFile(bucket, file) {
+  AWS.config.update(AWS_CREDENTIALS);
+
+  const params = {
+    Bucket: bucket,
+    Key: file,
+  };
+
+  return new Promise((resolve, reject) => {
+    new AWS.S3().getObject(params, (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(data.Body.toString()));
+      }
+    });
+  });
+}
+
+/**
+ * Method to upload a file to S3 storage
+ *
+ * @param {string} bucket The bucket to upload a file in
+ * @param {string} file The filename to upload to the bucket
+ * @param {JSON} data The data to put in the file
+ * @returns {Promise<any>} Response from S3
+ */
+async function uploadFile(bucket, file, data) {
+  AWS.config.update(AWS_CREDENTIALS);
+
+  const params = {
+    Bucket: bucket,
+    Key: file,
+    Body: JSON.stringify(data),
+  };
+
+  return new Promise((resolve, reject) => {
+    new AWS.S3().upload(params, (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
+
+export { downloadFile, uploadFile };
