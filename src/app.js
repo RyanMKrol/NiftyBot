4f1e6c3d9172bb54c449748db43907d96e68a918
RyanMKrol <ryankrol.m@gmail.com>
2021-08-29 21:24:24 +0100

    Add dynamo client login

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-process.stdout.write('Hello World!');
+import Discord from 'discord.js';
+
+import { DISCORD_CREDENTIALS } from './modules/constants';
+
+const client = new Discord.Client();
+
+client.login(DISCORD_CREDENTIALS.token);
---
+++ src/modules/constants/index.js
@@ -0,0 +1,8 @@
+// credential constants
+import DISCORD_CREDENTIALS from '../../../credentials/discord.json';
+import DYNAMO_CREDENTIALS from '../../../credentials/dynamo.json';
+
+// command constants
+const COMMAND_PREFIX = '!nb';
+
+export { DISCORD_CREDENTIALS, DYNAMO_CREDENTIALS, COMMAND_PREFIX };
