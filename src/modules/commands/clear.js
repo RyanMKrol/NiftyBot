cd53b58441719b846be9fdff3bb88dc4441d1e49
RyanMKrol <ryankrol.m@gmail.com>
2021-08-30 13:19:41 +0100

    Update command regexes to ensure command is the only text to match

--- src/modules/commands/clear.js
+++ src/modules/commands/clear.js
@@ -1,7 +1,7 @@
 import { COMMAND_PREFIX } from '../constants';
 import GUILD_MANAGER_COLLECTION from '../model';
 
-const IS_CLEAR_COMMAND_REGEX = `${COMMAND_PREFIX} clear`;
+const IS_CLEAR_COMMAND_REGEX = `^${COMMAND_PREFIX} clear`;
 
 /**
  * Handles the clear command
--- src/modules/commands/quit.js
+++ src/modules/commands/quit.js
@@ -1,7 +1,7 @@
 import { COMMAND_PREFIX } from '../constants';
 import GUILD_MANAGER_COLLECTION from '../model';
 
-const IS_QUIT_COMMAND_REGEX = `${COMMAND_PREFIX} quit`;
+const IS_QUIT_COMMAND_REGEX = `^${COMMAND_PREFIX} quit`;
 
 /**
  * Handles the quit command
