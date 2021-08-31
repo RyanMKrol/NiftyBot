35980f5558632f0a51a3c33c59bc3462834a1066
RyanMKrol <ryankrol.m@gmail.com>
2021-09-01 00:59:24 +0100

    Add resume, and skip command objects

---
+++ src/modules/commands/SkipCommand.js
@@ -0,0 +1,47 @@
+/* eslint-disable class-methods-use-this */
+
+import BaseCommand from './BaseCommand';
+
+import { COMMAND_PREFIX } from '../constants';
+import { GUILD_MANAGER_COLLECTION } from '../model';
+
+/**
+ * SkipCommand
+ */
+export class SkipCommand extends BaseCommand {
+  /**
+   * Constructor
+   */
+  constructor() {
+    const patterns = [`^${COMMAND_PREFIX}skip`];
+    super(patterns);
+  }
+
+  /**
+   * Process the skip command
+   *
+   * @param {module:app.Message} messageHook The original message hook
+   */
+  async process(messageHook) {
+    const guildId = messageHook.channel.guild.id;
+    const manager = await GUILD_MANAGER_COLLECTION.getManager(guildId);
+
+    manager.next();
+    manager.listSongs(messageHook);
+  }
+
+  /**
+   * Verifies that the current command is a skip command
+   *
+   * @param {string} command The command being used
+   * @returns {Array<any>} Array of parsed parameters
+   */
+  shouldHandle(command) {
+    const parsedValues = new RegExp(this.getPatterns()[0]).exec(command);
+    return parsedValues;
+  }
+}
+
+const SKIP = new SkipCommand();
+
+export default SKIP;
--- src/modules/commands/index.js
+++ src/modules/commands/index.js
@@ -3,3 +3,5 @@ export { default as LIST } from './ListCommand';
 export { default as PAUSE } from './PauseCommand';
 export { default as QUIT } from './QuitCommand';
 export { default as REMOVE } from './RemoveCommand';
+export { default as RESUME } from './ResumeCommand';
+export { default as SKIP } from './SkipCommand';
