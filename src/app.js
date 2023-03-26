758593136dbbdba4b81d40e171a0af2b5331e4b7
RyanMKrol <ryankrol.m@gmail.com>
2023-03-26 02:17:35 +0100

    Start discord bot using credentials

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import DISCORD_CREDENTIALS from './modules/constants';
 
-process.stdout.write(DISCORD_CREDENTIALS.token);
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(DISCORD_CREDENTIALS.token);
